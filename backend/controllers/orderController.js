4529ec8b233b3f27b53455a64c762a495696af0b
ShantanuPimpale <shantanunitinpimpale@gmail.com>
2024-05-28 12:55:06 +0530

    new

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -5,7 +5,7 @@ import Stripe from 'stripe';
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const placeorder = async (req, res) => {
-    const frontend_url = "http://localhost:5173";
+    const frontend_url = "https://onifood-project.netlify.app";
 
     try {
         // Create a new order in the database
